0b36086a22aef2df6b352ca1364f4f04d735f1e0
Lucifer <gj3372819612@163.com>
2024-11-30 13:16:22 +0800

    update "common.h"

--- common.h
+++ common.h
@@ -1,1 +1,19 @@
-#pragma once
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#include <iostream>
+#include <vector>
+#include <atomic>
+
+/** boost */
+#include <boost/noncopyable.hpp>
+
+#ifdef _WIN32
+#include <Windows.h>
+#endif
+
+#ifdef __linux__
+#include <unistd.h>
+#endif // __linux__
+
+#endif // !__COMMON_H__
